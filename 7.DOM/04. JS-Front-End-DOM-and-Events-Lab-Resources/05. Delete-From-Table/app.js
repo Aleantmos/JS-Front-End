0843b031ee7244f9b72fc9096fef24215d1fa8e9
Aleatmos <a.moskov95@gmail.com>
2023-03-27 13:26:52 +0300

    solutions

--- 7.DOM/04. JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
+++ 7.DOM/04. JS-Front-End-DOM-and-Events-Lab-Resources/05. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const result = document.getElementById("result");
+
+    const input = document.querySelector('name[name="email"]');
+    const evenTds = Array.from(document.querySelectorAll('td:nth-child(even)'));
+    let emailValue = input.value;
+
+    let foundElement = evenTds.find((td) => td.textContent === emailValue);
+
+    if (foundElement) {
+        foundElement.parentElement.remove();
+        result.textContent = "Deleted.";
+    } else {
+        result.textContent = "Not found."
+    }
 }
