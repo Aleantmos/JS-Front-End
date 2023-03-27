56ca8d924a4b142879e19422b327019f372ea324
Aleatmos <a.moskov95@gmail.com>
2023-03-27 13:56:58 +0300

    solution

--- 7.DOM/04. JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
+++ 7.DOM/04. JS-Front-End-DOM-and-Events-Lab-Resources/06. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+    const ulContainer = document.getElementById('items');
+    const input = document.getElementById("newItemText");
+
+    const newLi = document.createElement("li");
+    const newAnchor = document.createElement("a");
+
+    newLi.textContent = input.value;
+    newAnchor.textContent = "[Delete]";
+    
+    newAnchor.addEventListener("click", deleteHandler);
+
+    newLi.appendChild(newAnchor);
+
+    ulContainer.appendChild(newLi);
+    input.value = "";
+
+    function deleteHandler(e) {
+        const anchor = e.currentTarget;
+        anchor.parentElement.remove();
+    }
+
 }
