2777bd6114252b4a97ae8d5d7ff36e33fba4c887
Aleatmos <a.moskov95@gmail.com>
2023-07-17 17:32:26 +0300

    solutions

---
+++ _8_DOM_Events_Ex/05.Fill-Dropdown/dropdown.js
@@ -0,0 +1,19 @@
+function addItem() {
+
+    const newItemText = document.querySelector("#newItemText");
+    const newItemValue = document.querySelector("#newItemValue");
+
+    const menu = document.querySelector("#menu");
+
+    const option = document.createElement("option");
+    
+    option.textContent = newItemText.value;
+    option.value = newItemValue.value;
+
+    menu.appendChild(option);
+
+    newItemText.value = "";
+    newItemValue.value = "";
+
+
+}
