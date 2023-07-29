525789afdaa8b052fe93ecbf16a72434fae4868e
Aleatmos <a.moskov95@gmail.com>
2023-07-29 16:08:02 +0300

    p4

---
+++ _8_DOM_Events_Ex/more/04.Dynamic-Validation/app.js
@@ -0,0 +1,18 @@
+function validate() {
+
+    const email = document.querySelector('#email');
+
+    console.log(email);
+
+    function validateEmail() {
+        const output = email.value.match(/^[a-z]+@[a-z]+\.[a-z]+$/);
+
+        if (!output) {
+            email.classList.add('error');
+        } else {
+            email.classList.remove('error');
+        }
+    }
+
+    email.addEventListener('change', validateEmail);
+}
